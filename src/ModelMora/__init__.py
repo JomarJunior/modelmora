0820d86cbba3d9f93dca1a2818b900dcfb73a95d
JomarJunior <jomarjunior@poli.ufrj.br>
2025-11-20 00:56:46 -0300

    Add initial __init__.py files for main and test directories

---
+++ src/ModelMora/__init__.py
@@ -0,0 +1,7 @@
+"""
+ModelMora - A dedicated neural network model management and inference serving microservice.
+
+This package provides functionalities to manage and serve neural network models via RESTful APIs, gRPC, and Kafka.
+"""
+
+__version__ = "0.1.0"
